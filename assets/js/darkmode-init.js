2e2ecb3c31c0d5c18edf3f3f1c1300614c37f51f
Yuyang Wang <wangyuyangyuyang@gmail.com>
2022-11-09 09:23:49 -0500

    update

--- assets/js/darkmode-init.js
+++ assets/js/darkmode-init.js
@@ -1,17 +1,17 @@
 const globalDark =
   window.matchMedia &&
-  window.matchMedia("(prefers-color-scheme: dark)").matches;
-const localMode = localStorage.getItem("theme");
+  window.matchMedia('(prefers-color-scheme: dark)').matches;
+const localMode = localStorage.getItem('theme');
 
 if (globalDark && localMode === null) {
-  localStorage.setItem("theme", "dark");
-  document.documentElement.setAttribute("data-dark-mode", "");
+  localStorage.setItem('theme', 'dark');
+  document.documentElement.setAttribute('data-dark-mode', '');
 }
 
-if (globalDark && localMode === "dark") {
-  document.documentElement.setAttribute("data-dark-mode", "");
+if (globalDark && localMode === 'dark') {
+  document.documentElement.setAttribute('data-dark-mode', '');
 }
 
-if (localMode === "dark") {
-  document.documentElement.setAttribute("data-dark-mode", "");
+if (localMode === 'dark') {
+  document.documentElement.setAttribute('data-dark-mode', '');
 }
